8c035f09b75202f7887e1bb9ac29da78715afe08
Klymochko-Mykhailo <kmisa5106@gmail.com>
2025-12-27 13:09:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,41 @@ def error_example():
         logging.error(f"Сталася помилка: {e}")
 
 error_example()
+
+# Завдання 3
+
+def login(username, password):
+    try:
+        assert username == "kiril" and password == "1234", \
+            "Невірне ім'я користувача або пароль"
+        print("Вхід виконано успішно")
+    except AssertionError as e:
+        print(e)
+
+login("kiril", "1234")
+login("user", "0000")
+
+# Завдання 4
+
+def check_age(age):
+    try:
+        assert age >= 18, "Вам має бути 18 років або більше"
+        print("Ви можете використовувати цей сервіс")
+    except AssertionError as e:
+        print(e)
+
+check_age(20)
+check_age(15)
+
+# Завдання 5
+
+def process_list(input_list):
+    try:
+        assert len(input_list) >= 3, \
+            "Список повинен містити принаймні 3 елементи"
+        print(f"Список містить {len(input_list)} елементів")
+    except AssertionError as e:
+        print(e)
+
+process_list([1, 2, 3, 4])
+process_list([1])
